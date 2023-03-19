b0f269f4be7631145ebd905e07524a2e74b3163b
Nilesh Bhutda <bhutdanilesh2517@gmail.com>
2023-03-20 01:02:35 +0530

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,32 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UserModule } from './user/user.module';
 import { NavComponent } from './nav/nav.component';
+import {AngularFireModule} from '@angular/fire/compat';
+import { environment } from 'src/environments/environment';
+import {AngularFireAuthModule} from '@angular/fire/compat/auth';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { VideoModule } from './video/video.module';
+import { UploadComponent } from './upload/upload.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavComponent
+    NavComponent,
+    HomeComponent,
+    AboutComponent,
+    UploadComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    UserModule
+    UserModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFireAuthModule,
+    BrowserAnimationsModule,
+    VideoModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    production: true,
+    firebase: {
+        apiKey: "AIzaSyB2ORDgNXBDiW1NqpaVaO6bLLk6VoCzTyo",
+        authDomain: "clipsz.firebaseapp.com",
+        projectId: "clipsz",
+        storageBucket: "clipsz.appspot.com",
+        messagingSenderId: "775098430502",
+        appId: "1:775098430502:web:27fe4ceb514b324ca962e7"
+      }
+};
